3e253e37811492e41d3c2c93db269cad162d507c
amir.rashed <amir.rashed@00IT668HL.EG01.Etisalat.net>
2016-04-10 16:42:58 +0200

    The change is only in the Entry class, the comment block

--- src/generateldappeeraddingand/enabling/Entry.java
+++ src/generateldappeeraddingand/enabling/Entry.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+This is the entry object in the Project
  */
 package generateldappeeraddingand.enabling;
 
